6f54efec31dbeb667e8d99867fca19f71d05ccdc
Kevin Green <Greenkevin28@icloud.com>
2025-10-20 13:42:12 -0230

    Created instance variables and constructor for Patient.java

--- Patient.java
+++ Patient.java
@@ -1,4 +1,17 @@
 
-public class Patient {
+import java.util.ArrayList;
 
+public class Patient extends Person {
+
+    // Instance variables
+    private ArrayList<Medication> medications;
+    private ArrayList<Prescription> prescriptions;
+
+    // Constructor
+    public Patient(String ID, String name, String age, String phoneNumber) {
+
+        super(ID, name, age, phoneNumber);
+        this.medications = new ArrayList<>();
+        this.prescriptions = new ArrayList<>();
+    }
 }
