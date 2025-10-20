4bff9eba564059b88d7ad0e1105855c6a1986ab4
Kevin Green <Greenkevin28@icloud.com>
2025-10-20 12:44:47 -0230

    Created instance variables and constructor for Person.java

---
+++ Person.java
@@ -0,0 +1,19 @@
+
+public abstract class Person {
+
+    // Instance variables
+    private String ID;
+    private String name;
+    private String age;
+    private String phoneNumber;
+
+    // Constrcutor
+    Person(String ID, String name, String age, String phoneNumber) {
+
+        this.ID = ID;
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+
+}
