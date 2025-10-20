2fbb3c1ec1c123f30808742d07e6dc5bea588e74
BigSponce <BigSponce@gmail.com>
2025-10-20 12:55:40 -0230

    Updated Medication.java with instance variables, constructor, getters and setters

--- Medication.java
+++ Medication.java
@@ -1,4 +1,47 @@
+import java.time.LocalDate;
 
 public class Medication {
+    private int ID;
+    private String name;
+    private int quantity;
+    private LocalDate expiryDate;
 
+    Medication(int ID, String name, int quantity, LocalDate expiryDate) {
+        this.ID = ID;
+        this.name = name;
+        this.quantity = quantity;
+        this.expiryDate = expiryDate;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public void setID(int iD) {
+        ID = iD;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
