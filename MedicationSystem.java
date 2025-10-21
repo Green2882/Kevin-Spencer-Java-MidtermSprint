b0ba47e5fa2cb872c800500a40655163b9ad1751
Kevin Green <Greenkevin28@icloud.com>
2025-10-21 13:07:48 -0230

    Added instance variables and constructor to MedicationSystem.java

--- MedicationSystem.java
+++ MedicationSystem.java
@@ -1,4 +1,20 @@
 
+import java.util.ArrayList;
+
 public class MedicationSystem {
 
+    // Instance variables
+    private ArrayList<Patient> patients;
+    private ArrayList<Doctor> doctors;
+    private ArrayList<Medication> medications;
+    private ArrayList<Prescription> prescriptions;
+
+    // Constructor
+    public MedicationSystem() {
+
+        patients = new ArrayList<>();
+        doctors = new ArrayList<>();
+        medications = new ArrayList<>();
+        prescriptions = new ArrayList<>();
+    }
 }
