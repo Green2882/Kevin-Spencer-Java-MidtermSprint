91770bad4e09c5dd891639eb2c42456252e28170
BigSponce <BigSponce@gmail.com>
2025-10-21 12:44:25 -0230

    Added ArrayList to Doctor.java. Starting on menu creation.

--- Doctor.java
+++ Doctor.java
@@ -1,13 +1,14 @@
+import java.util.ArrayList;
 
 public class Doctor extends Person {
 
     // Instance Variables
     private String specialization;
-    private Patient[] patients;
+    private ArrayList<Patient> patients;
 
-    Doctor(String ID, String name, String age, String phoneNumber, String specialization, Patient[] patients) {
+    Doctor(String ID, String name, String age, String phoneNumber, String specialization) {
         super(ID, name, age, phoneNumber);
         this.specialization = specialization;
-        this.patients = patients;
+        this.patients = new ArrayList<>();
     }
 }
