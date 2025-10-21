394617cbfe11a56361fb286678b4e0ff08178541
Kevin Green <Greenkevin28@icloud.com>
2025-10-21 12:34:01 -0230

    Added instance variables and constrcutor to Prescription.java

--- Prescription.java
+++ Prescription.java
@@ -1,4 +1,22 @@
 
+import java.time.LocalDate;
+
 public class Prescription {
 
+    // Instance variables
+    private int ID;
+    private Doctor doctor;
+    private Patient patient;
+    private Medication medication;
+    private LocalDate prescriptionExp;
+
+    // Constructor
+    public Prescription(int ID, Doctor doctor, Patient patient, Medication medication, LocalDate prescriptionExp) {
+
+        this.ID = ID;
+        this.doctor = doctor;
+        this.patient = patient;
+        this.medication = medication;
+        this.prescriptionExp = prescriptionExp;
+    }
 }
