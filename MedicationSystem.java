cb4cbd0dca5ac51c740f9e89ab1662f3c7e79899
Kevin Green <Greenkevin28@icloud.com>
2025-10-21 13:23:05 -0230

    added Methods to add/delete patients/medications/doctors

--- MedicationSystem.java
+++ MedicationSystem.java
@@ -17,4 +17,35 @@ public class MedicationSystem {
         medications = new ArrayList<>();
         prescriptions = new ArrayList<>();
     }
+
+    // Methods to add/delete patients/medications/doctors
+    public void addPatient(Patient patient) {
+
+        patients.add(patient);
+    }
+
+    public void deletePatient(Patient patient) {
+
+        patients.remove(patient);
+    }
+
+    public void addMedication(Medication medication) {
+
+        medications.add(medication);
+    }
+
+    public void deleteMedication(Medication medication) {
+
+        medications.remove(medication);
+    }
+
+    public void addDoctor(Doctor doctor) {
+
+        doctors.add(doctor);
+    }
+
+    public void deleteDoctor(Doctor doctor) {
+
+        doctors.remove(doctor);
+    }
 }
